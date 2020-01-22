7c3f0f1c3359aeb3aa1f88cc549e2a03c1984f87
Babydha Nagarajane <Babydha.Nagarajane@Rencata.com>
2020-01-21 23:33:52 -0800

    New file added in RepoB

---
+++ WorkFlowTwoConsoleApp/RepoBFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkFlowTwoConsoleApp
+{
+    class RepoBFile
+    {
+        void Display()
+        {
+            Console.WriteLine("Repo B file");
+        }
+    }
+}
